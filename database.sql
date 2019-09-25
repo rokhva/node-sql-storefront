55305f19f74e40f904c733dd72267f9635e93708
Bahaar Rokhva <rokhvab@gmail.com>
2019-09-25 14:32:12 -0600

    added fake starter data

--- database.sql
+++ database.sql
@@ -9,3 +9,18 @@ CREATE TABLE products (
     price DECIMAL (50) NULL,
     stock_quantity INTEGER (50) NULL,
 );
+
+INSERT INTO authors (product_name, department_name, price, stock_quantity) 
+VALUES ('sloth hugs', 'pets', '50', '100'),
+       ('sloth kisses', 'pets', '100', '100'),
+       ('mugs', 'kitchen', '10', '50'),
+       ('sporks', 'kitchen', '3', '70'),
+       ('insense', 'just granola things', '15', '50'),
+       ('yoga mat', 'just granola things', '100', '20'),
+       ('loose leaf tea', 'pantry', '8', '20'),
+       ('ground coffee', 'pantry', '10', '40'),
+       ('whole bean coffee', 'pantry', '18', '100'),
+       ('pour over filter', 'kitchen', '6', '25');
+
+
+
