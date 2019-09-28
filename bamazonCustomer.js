97bae6d7ffbe24694dda05391eb44c0443a593d6
Bahaar Rokhva <rokhvab@gmail.com>
2019-09-27 19:27:20 -0600

    all of the functions are working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,20 +13,81 @@ var connection = mysql.createConnection({
     user: "root",
   
     // Your password
-    password: "",
+    password: "Edsheeran1//",
     database: "bamazon"
   });
   
   connection.connect(function(err) {
     if (err) throw err;
     // runSearch();
-    
+    start()
   });
 
+function start(){
+  let query = "SELECT * FROM products";
 
-let query = "SELECT * FROM products";
-
-connection.query(query, function (err,results){
+  connection.query(query, function (err,results){
     if(err) return console.log(err);
     console.table(results);
-})
+    ask();
+  })
+}
+
+function ask(){
+  inquirer
+      .prompt([
+        {
+          type: "number",
+          name: "id",
+          message: "what's the id of the item you want to purchase?"
+        },
+        {
+          type: "number",
+          name: "quantity",
+          message: "how many of them do you want?"
+        }
+      ])
+      .then(function(val) {
+        let query = `SELECT stock_quantity FROM products WHERE id = ${val.id}`;
+
+        connection.query(query, function (err,results){
+          if(err) return console.log(err);
+          //console.log("results: ",results);
+          let quantity = results[0].stock_quantity;
+          if(quantity >= val.quantity){
+            purchase(quantity, val.quantity, val.id)
+          }
+          else{
+            console.log("there's not enough sry")
+            connection.end();
+          }
+        })
+      });
+}
+
+function purchase(currentQuantity, requestedQuantity, id){
+    let stockQuery = `UPDATE products SET stock_quantity = ${currentQuantity - requestedQuantity} WHERE id = ${id}`;
+    connection.query(stockQuery, function (err,results){
+    if(err) return console.log(err);
+    anotherId();
+  })
+}
+
+function anotherId(){
+    inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "anotherPurchase",
+        message: "Would you like to buy another item (Y/N)?"
+      }
+    ])
+    .then(function(val){
+        console.log(val);
+        if(val.anotherPurchase = true){
+            start();
+        }
+    })
+}
+
+
